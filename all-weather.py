967473a6dce5379294549918f3a7c7e69aad94f7
Andrew Han <handrew11@gmail.com>
2016-09-03 02:31:50 -0700

    better systemization -- can now just adjust the constants at the top to toggle

--- all-weather.py
+++ all-weather.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import pandas_datareader.data as web
 import datetime
-import pdb
 import numpy as np
+import collections
+
+import pdb
 import sys
 import modules.util as util
 import pprint
@@ -24,7 +26,15 @@ import pprint
 # by subtracting out covariance as well, since covariance itself is unstable over time
 
 WEIGHTS_FILE = "output/weights.csv"
-TICKERS = {"stocks": ['VTI'], "commodities": ['DBC'], "nominal bonds": ['TLT'], "inflation": ['GLD']}
+
+TICKERS = {
+	"stocks": ['VTI'], 
+	"commodities": ['DBC'], 
+	"corporate credit": [], # empty for now, can add
+	'EM credit': [],
+	"nominal bonds": ['TLT'], 
+	"inflation-linked": ['GLD']
+}
 
 def main():
 	pp = pprint.PrettyPrinter(indent=4)
@@ -42,9 +52,9 @@ def main():
 	weight_dict = finalize_ticker_weights(asset_class_weights, environment_weights, box_weights)
 
 	print "Box weights"
-	print pp.pprint(box_weights)
+	pp.pprint(box_weights)
 	print "Environment weights"
-	print pp.pprint(environment_weights)
+	pp.pprint(environment_weights)
 	print "Final weights"
 	pp.pprint(weight_dict)
 
@@ -57,21 +67,32 @@ def update_weight_file(weight_dict):
 	weights = pd.DataFrame(weights)
 	weights.to_csv(WEIGHTS_FILE, index=False)
 
-def equalize_weights(args):
-	num_args = len(args)
-	last_vol = args[num_args-1]
+# given a tuple of (label, value), return
+# risk parity equalized {"label": weight}
+def equalize_weights(tuples):
+	tuples = [tup for tup in tuples if tup[1] != 0.0] # remove zero values
+	num_args = len(tuples)
 
-	last_vol_over_other_vols = []
-	for i in range(0, num_args-1): 
-		last_vol_over_other_vols.append(last_vol/args[i])
+	if (num_args):
+		last_vol = tuples[num_args-1][1]
+		last_label = tuples[num_args-1][0]
 
-	weight_n = 1.0 / (sum(last_vol_over_other_vols) + 1)
-	weights_i = []
-	for i in range(0, num_args-1):
-		weights_i.append((last_vol / args[i]) * weight_n)
+		last_vol_over_other_vols = []
+		for i in range(0, num_args-1): 
+			curr_vol = tuples[i][1]
+			last_vol_over_other_vols.append(last_vol/curr_vol)
 
-	weights_i.append(weight_n)
-	return weights_i
+		weight_n = 1.0 / (sum(last_vol_over_other_vols) + 1)
+		weights_i = collections.defaultdict(lambda: 0.0)
+		for i in range(0, num_args-1):
+			curr_vol = tuples[i][1]
+			curr_label = tuples[i][0]
+			weights_i[curr_label] = (last_vol / curr_vol) * weight_n
+
+		weights_i[last_label] = weight_n
+		return weights_i
+	else:
+		return collections.defaultdict(lambda: 0.0)
 
 def get_ticker_volatilities(ticker_data):
 	ticker_volatilities = {}
@@ -84,13 +105,17 @@ def finalize_ticker_weights(asset_class_weights, environment_weights, box_weight
 	stocks_weight = environment_weights['gr'] * box_weights['gr']['stocks'] + environment_weights['if'] * box_weights['if']['stocks'] 
 	commodities_weight = environment_weights['gr'] * box_weights['gr']['commodities'] + environment_weights['ir'] * box_weights['ir']['commodities']
 	nominal_bonds_weight = environment_weights['gf'] * box_weights['gf']['nominal bonds'] + environment_weights['if'] * box_weights['if']['nominal bonds']
-	inflation_weight = environment_weights['ir'] * box_weights['ir']['inflation'] + environment_weights['gf'] * box_weights['gf']['inflation']
+	inflation_weight = environment_weights['ir'] * box_weights['ir']['inflation-linked'] + environment_weights['gf'] * box_weights['gf']["inflation-linked"]
+	em_credit_weight = environment_weights['ir'] * box_weights['ir']['EM credit'] + environment_weights['gr'] * box_weights['gr']["EM credit"]
+	corporate_credit_weight = environment_weights['gr'] * box_weights['gr']['corporate credit']
 
 	weights_by_asset = {
 		"stocks": stocks_weight, 
 		"commodities": commodities_weight, 
 		"nominal bonds": nominal_bonds_weight,
-		"inflation": inflation_weight
+		"inflation-linked": inflation_weight,
+		"EM credit": em_credit_weight,
+		"corporate credit": corporate_credit_weight
 	}
 
 	weights_dict = {}
@@ -118,10 +143,10 @@ def get_ticker_data():
 # return {asset_class: {tickers: weights}}
 def get_asset_class_weights(ticker_volatilities):
 	asset_class_weights = {}
-	for asset_class in TICKERS:
+	for asset_class in TICKERS: # stocks, commodities, EM credit, etc
 		tickers_in_asset_class = TICKERS[asset_class]
 		volatilities_for_tickers = [ticker_volatilities[ticker] for ticker in tickers_in_asset_class]
-		ordered_weights_by_ticker = equalize_weights(volatilities_for_tickers)
+		ordered_weights_by_ticker = equalize_weights(zip(tickers_in_asset_class, volatilities_for_tickers)).values()
 		asset_class_weights[asset_class] = dict(zip(tickers_in_asset_class, ordered_weights_by_ticker))
 
 	return asset_class_weights
@@ -142,50 +167,59 @@ def get_asset_class_volatilities_from_ticker_weights(asset_class_weights, ticker
 def get_box_weights(ticker_volatilities, asset_class_weights):
 	asset_volatilities = get_asset_class_volatilities_from_ticker_weights(asset_class_weights, ticker_volatilities)
 
+	# hacky i know but agh...
+	def get_volatilities_tuples_if_exists(wanted_assets, asset_vol_dict): 
+		return [(a, asset_vol_dict[a]) for a in wanted_assets if a in asset_vol_dict]
+
 	# growth rising
-	gr_weights = equalize_weights([asset_volatilities['stocks'], asset_volatilities['commodities']])
-	stocks_weight_gr = gr_weights[0]
-	commodities_weight_gr = gr_weights[1]
+	gr_weights = equalize_weights(get_volatilities_tuples_if_exists(['stocks', 'commodities', 'EM credit', 'corporate credit'], asset_volatilities))
+	stocks_weight_gr = gr_weights['stocks']
+	commodities_weight_gr = gr_weights['commodities']
+	em_credit_weight_gr = gr_weights['EM credit']
+	corp_credit_weight_gr = gr_weights['corporate credit']
 
 	# growth falling
-	gf_weights = equalize_weights([asset_volatilities['nominal bonds'], asset_volatilities['inflation']])
-	nominal_bonds_weight_gf = gf_weights[0]
-	inflation_weight_gf = gf_weights[1]
+	gf_weights = equalize_weights(get_volatilities_tuples_if_exists(['nominal bonds', 'inflation-linked'], asset_volatilities))
+	nominal_bonds_weight_gf = gf_weights['nominal bonds']
+	inflation_weight_gf = gf_weights['inflation-linked']
 
 	# inflation rising
-	ir_weights = equalize_weights([asset_volatilities['inflation'], asset_volatilities['commodities']])
-	inflation_weight_ir = ir_weights[0]
-	commodities_weight_ir = ir_weights[1]
+	ir_weights = equalize_weights(get_volatilities_tuples_if_exists(['inflation-linked', 'commodities', 'EM credit'], asset_volatilities))
+	inflation_weight_ir = ir_weights['inflation-linked']
+	commodities_weight_ir = ir_weights['commodities']
+	em_credit_weight_ir = ir_weights['EM credit']
 
 	# inflation falling
-	if_weights = equalize_weights([asset_volatilities['stocks'], asset_volatilities['nominal bonds']])
-	stocks_weight_if = if_weights[0]
-	nominal_bonds_weight_if = if_weights[1]
+	if_weights = equalize_weights(get_volatilities_tuples_if_exists(['stocks', 'nominal bonds'], asset_volatilities))
+	stocks_weight_if = if_weights['stocks']
+	nominal_bonds_weight_if = if_weights['nominal bonds']
 
 	return {
-		"gr": {"stocks": stocks_weight_gr, "commodities": commodities_weight_gr},
-		"gf": {"nominal bonds": nominal_bonds_weight_gf, "inflation": inflation_weight_gf},
-		"ir": {"inflation": inflation_weight_ir, "commodities": commodities_weight_ir},
+		"gr": {"stocks": stocks_weight_gr, "commodities": commodities_weight_gr, "EM credit": em_credit_weight_gr, "corporate credit": corp_credit_weight_gr},
+		"gf": {"nominal bonds": nominal_bonds_weight_gf, "inflation-linked": inflation_weight_gf},
+		"ir": {"inflation-linked": inflation_weight_ir, "commodities": commodities_weight_ir, "EM credit": em_credit_weight_ir},
 		"if": {"nominal bonds": nominal_bonds_weight_if, "stocks": stocks_weight_if},
 	}
 
 def get_environment_weights(ticker_volatilities, weights_per_asset_class, weights_per_box):
 	asset_volatilities = get_asset_class_volatilities_from_ticker_weights(weights_per_asset_class, ticker_volatilities)
-
 	gr_vol = weights_per_box['gr']['stocks'] * asset_volatilities['stocks'] \
-			+ weights_per_box['gr']['commodities'] * asset_volatilities['commodities']
+			+ weights_per_box['gr']['commodities'] * asset_volatilities['commodities'] \
+			+ weights_per_box['gr']['EM credit'] * asset_volatilities['EM credit'] \
+			+ weights_per_box['gr']['corporate credit'] * asset_volatilities['corporate credit']
 	gf_vol = weights_per_box['gf']['nominal bonds'] * asset_volatilities['nominal bonds'] \
-			+ weights_per_box['gf']['inflation'] * asset_volatilities['inflation']
-	ir_vol = weights_per_box['ir']['inflation'] * asset_volatilities['inflation'] \
-			+ weights_per_box['ir']['commodities'] * asset_volatilities['commodities']
+			+ weights_per_box['gf']["inflation-linked"] * asset_volatilities["inflation-linked"]
+	ir_vol = weights_per_box['ir']["inflation-linked"] * asset_volatilities["inflation-linked"] \
+			+ weights_per_box['ir']['commodities'] * asset_volatilities['commodities'] \
+			+ weights_per_box['ir']['EM credit'] * asset_volatilities['EM credit']
 	if_vol = weights_per_box['if']['stocks'] * asset_volatilities['stocks'] \
 			+ weights_per_box['if']['nominal bonds'] * asset_volatilities['nominal bonds']
 
-	environment_weights = equalize_weights([gr_vol, gf_vol, ir_vol, if_vol])
-	gr_weight = environment_weights[0]
-	gf_weight = environment_weights[1]
-	ir_weight = environment_weights[2]
-	if_weight = environment_weights[3]
+	environment_weights = equalize_weights([('gr', gr_vol), ('gf', gf_vol), ('ir', ir_vol), ('if', if_vol)])
+	gr_weight = environment_weights['gr']
+	gf_weight = environment_weights['gf']
+	ir_weight = environment_weights['ir']
+	if_weight = environment_weights['if']
 
 	return { 
 		"gr": gr_weight,
