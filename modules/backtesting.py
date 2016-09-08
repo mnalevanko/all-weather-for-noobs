fd39f3a22a9cb456306d42f147116a3d6ab387ae
Andrew Han <handrew11@gmail.com>
2016-09-08 14:39:55 -0700

    make copy so you don't modify the actual dict

--- modules/backtesting.py
+++ modules/backtesting.py
@@ -5,24 +5,25 @@ import datetime
 
 def backtest(weight_dict, output=False):
 	print "Backtesting..."
-	del weight_dict['Date'] 
+	new_weight_dict = weight_dict.copy()
+	del new_weight_dict['Date'] 
 
 	start = datetime.datetime(1940, 1, 1)
 	end = datetime.datetime.now()
 
 	returns_dfs = []
 
-	for ticker in weight_dict:
+	for ticker in new_weight_dict:
 		df = util.get_returns(ticker, start, end, period=1)
 		df['%s Returns' % ticker] = df['Weekly Returns']
 		df = pd.DataFrame(df['%s Returns' % ticker])
 		returns_dfs.append(df)
 
 	merged_df = merge_dataframes_by_latest_start_date(returns_dfs)
-	merged_df['Portfolio Returns'] = merged_df['%s Returns' % weight_dict.keys()[0]] * 0.0 # just set it to 0 
+	merged_df['Portfolio Returns'] = merged_df['%s Returns' % new_weight_dict.keys()[0]] * 0.0 # just set it to 0 
 
-	for ticker in weight_dict:
-		weight = weight_dict[ticker]
+	for ticker in new_weight_dict:
+		weight = new_weight_dict[ticker]
 		merged_df['Portfolio Returns'] = merged_df['Portfolio Returns'] + weight * merged_df['%s Returns' % ticker]
 
 	if (output): merged_df.to_csv("backtest_results.csv")
