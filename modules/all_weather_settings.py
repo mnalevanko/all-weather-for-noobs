b1d8c4edeb344a338bbfef89437d28ed457e9134
Andrew Han <handrew11@gmail.com>
2016-10-03 13:57:26 -0700

    commented

--- modules/all_weather_settings.py
+++ modules/all_weather_settings.py
@@ -15,19 +15,24 @@ TICKERS = {
 	"inflation-linked": ['GLD']
 }
 
+# VTI, VWO, VGK, DBC, HYG, TLT, GLD
 TIER_1_TICKERS = TICKERS.copy() 
 
+# VTI, VWO, VGK, DBC, TLT, GLD
 TIER_2_TICKERS = TICKERS.copy()
 TIER_2_TICKERS['corporate credit'] = []
 
+# VTI, DBC, TLT, GLD
 TIER_3_TICKERS = TICKERS.copy()
 TIER_3_TICKERS['corporate credit'] = []
 TIER_3_TICKERS['stocks'] = ['VTI']
 
+# VTI, VGK, TLT, GLD, DBC
 TIER_4_TICKERS = TICKERS.copy()
 TIER_4_TICKERS['corporate credit'] = []
 TIER_4_TICKERS['stocks'] = ['VTI', 'VGK']
 
+# VTI, VGK, HYG, TLT, GLD, DBC
 TIER_5_TICKERS = TICKERS.copy()
 TIER_5_TICKERS['stocks'] = ['VTI', 'VGK']
 
@@ -44,7 +49,6 @@ TICKER_TIERS = [
 
 TIER_CHOICE = 5
 TICKER_VOLATILITY_OVERRIDES = get_implied_volatilities_for_tickers(['TLT', 'GLD', 'DBC', 'HYG', 'VTI', 'VWO', 'VGK'])
-# TICKER_VOLATILITY_OVERRIDES = {}
 VOL_WINDOW = 252
 
 ###############################################
